7c0fd6c5c7ea055c4f988631f6cf11cd66d3559b
David Zheng <dav.d.zhng@gmail.com>
2020-10-07 15:20:12 -0400

    Actual Initial Commit
    idk what i'm doing

---
+++ src/main/java/blocksnbrickstest/test/Test.java
@@ -0,0 +1,10 @@
+package blocksnbrickstest.test;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Test implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
